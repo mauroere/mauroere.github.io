c000958fdae3f92874449cb1ab234a049300b5e5
Mauro Rementeria <mauroere@gmail.com>
2021-06-04 21:01:52 -0300

    .

--- LaEmpanaderia_RementeriaMauro/src/App.js
+++ LaEmpanaderia_RementeriaMauro/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Clase 2 - Trabajo entregable.</p>
+        <p>Clase 2 - Trabajo entregable</p>
         <a
           className="App-link"
           href="https://reactjs.org"
